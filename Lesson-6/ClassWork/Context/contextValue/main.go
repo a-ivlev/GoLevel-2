cb51197cffa54bad5b40353565989feeb743e975
Aleksandr <a.y.ivlev@gmail.com>
2022-03-04 21:39:23 +0300

    Passing values by context added comment

--- Lesson-6/ClassWork/Context/contextValue/main.go
+++ Lesson-6/ClassWork/Context/contextValue/main.go
@@ -12,7 +12,9 @@ func main() {
 	ctx1 := context.WithValue(ctx0, "key", "value1")
 	ctx2 := context.WithValue(ctx1, "Geek", "Brains")
 
-	fmt.Println("ctx0 key =", ctx0.Value("key"))
-	fmt.Println("ctx1 key =", ctx1.Value("key"))
-	fmt.Println("ctx2 key =", ctx2.Value("key"), "ctx2 Geek =", ctx2.Value("Geek"))
+	// Context штука слоистая, и мы не перезатираем слой, а добавляем слой.
+	// И на предыдущих слоях остаётся всё попрежнему.
+	fmt.Println("ctx0 key =", ctx0.Value("key")) // ctx0 key = value
+	fmt.Println("ctx1 key =", ctx1.Value("key")) // ctx1 key = value1
+	fmt.Println("ctx2 key =", ctx2.Value("key"), "ctx2 Geek =", ctx2.Value("Geek")) // ctx2 key = value1 ctx2 Geek = Brains
 }
